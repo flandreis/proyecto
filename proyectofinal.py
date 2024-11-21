b6455fb1b5924a2fe8f36658f465dcfa2fb6bfe5
flandreis <145865405+flandreis@users.noreply.github.com>
2024-11-21 15:27:12 -0700

    Update proyectofinal.py

--- proyectofinal.py
+++ proyectofinal.py
@@ -12,11 +12,12 @@ if opcion == "Proteínas":
         st.title("Descripción")
         st.write("blablablaba")
         st.write("Colágeno estructura tipo I")
-	image_comparison(
+image_comparison(
     	img1="Col_T1.jpg",
 		img2="colágeno_TIPO1.jpg",
 			label1="Estructura molecular",
-			label2="Estructura",)
+			label2="Estructura",
+)
 elif opcion == "TIPO I":
 		st.write("Has seleccionado Tipo I")
 elif opcion == TIPO II:
@@ -26,12 +27,12 @@ elif opcion == "TIPO III":
 		st.title("ESTRUCTURAS TIPO III")
 		st.write("bkbalblaba")
 		st.write("Colágeno estructura tipo III")
-	image_comparison(
+image_comparison(
 		img1="estructuracolag3.jpg",
 		img2="Colag.Tipo3.jpg",
 			label1="Estructura molecular  ",
 			label2="Estructura",
-        )
+)
 else:
 		st.write("Selecciona una opción del menú.")
 
