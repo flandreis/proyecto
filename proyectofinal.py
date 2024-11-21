30551542780573bf5e85dfd2eb1f5ac5dc691c6f
flandreis <145865405+flandreis@users.noreply.github.com>
2024-11-21 15:26:01 -0700

    Update proyectofinal.py

--- proyectofinal.py
+++ proyectofinal.py
@@ -12,28 +12,27 @@ if opcion == "Proteínas":
         st.title("Descripción")
         st.write("blablablaba")
         st.write("Colágeno estructura tipo I")
-image_comparison(
-    img1="Col_T1.jpg",
-      img2="colágeno_TIPO1.jpg",
-       label1="Estructura molecular",
-        label2="Estructura",)
-
-     elif opcion == "TIPO I":
-            st.write("Has seleccionado Tipo I")
-     elif opcion == TIPO II:
-            st.write("Has seleccionado Tipo II")
-     elif opcion == "TIPO III":
-            st.write("Has seleccionado TIPO III")
-            st.title("ESTRUCTURAS TIPO III")
-            st.write("bkbalblaba")
-            st.write("Colágeno estructura tipo III")
-        image_comparison(
-          img1="estructuracolag3.jpg",
-          img2="Colag.Tipo3.jpg",
-           label1="Estructura molecular  ",
-            label2="Estructura",
+	image_comparison(
+    	img1="Col_T1.jpg",
+		img2="colágeno_TIPO1.jpg",
+			label1="Estructura molecular",
+			label2="Estructura",)
+elif opcion == "TIPO I":
+		st.write("Has seleccionado Tipo I")
+elif opcion == TIPO II:
+		st.write("Has seleccionado Tipo II")
+elif opcion == "TIPO III":
+		st.write("Has seleccionado TIPO III")
+		st.title("ESTRUCTURAS TIPO III")
+		st.write("bkbalblaba")
+		st.write("Colágeno estructura tipo III")
+	image_comparison(
+		img1="estructuracolag3.jpg",
+		img2="Colag.Tipo3.jpg",
+			label1="Estructura molecular  ",
+			label2="Estructura",
         )
-      else:
-            st.write("Selecciona una opción del menú.")
+else:
+		st.write("Selecciona una opción del menú.")
 
 #streamlit run proyectofinal.py
