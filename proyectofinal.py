19b5fbdac041f5d79573f7d3116dbac87eae3710
flandreis <145865405+flandreis@users.noreply.github.com>
2024-11-21 17:11:14 -0700

    Update proyectofinal.py

--- proyectofinal.py
+++ proyectofinal.py
@@ -5,24 +5,43 @@ st.set_page_config(
     page_title="DASHBOARD PROTEINAS",
     page_icon="🧬",
     layout="centered")
-#menu["Proteínas", "TIPO I", "TIPO II", "TIPO III", "TIPO IV"]
-#opcion = st.sidebar.selectbox("ÍNDICE DE ESTRUCTURAS",menu)
-#st.sidebar.caption("descripcion breve ajaa")
-import argparse
-
-parser = argparse.ArgumentParser(description="Menú interactivo en terminal")
-parser.add_argument("opcion", type=str, help="Selecciona una opción: Proteínas, TIPO I, TIPO II, TIPO III")
-
-args = parser.parse_args()
-
-if args.opcion == "Proteínas":
-    print("Contenido sobre proteínas.")
-elif args.opcion == "TIPO I":
-    print("Descripción de la estructura TIPO I.")
-elif args.opcion == "TIPO II":
-    print("Descripción de la estructura TIPO II.")
+menu["Proteínas", "TIPO I", "TIPO II", "TIPO III", "TIPO IV"]
+opcion = st.sidebar.selectbox("ÍNDICE DE ESTRUCTURAS",menu)
+st.sidebar.caption("descripcion breve ajaa")
+if opcion == "Proteínas":
+    st.title("Descripción")
+    st.write("blablablaba")
+    st.write("Colágeno estructura tipo I")
+    image_comparison(
+        img1="Col_T1.jpg",
+        img2="colágeno_TIPO1.jpg",
+        label1="Estructura molecular",
+        label2="Estructura",
+    )
+elif opcion == "TIPO I":
+    st.write("Has seleccionado Tipo I")
+    st.write("Colágeno estructura tipo I")
+    image_comparison(
+        img1="Col_T1.jpg",
+        img2="colágeno_TIPO1.jpg",
+        label1="Estructura molecular",
+        label2="Estructura",
+    )
+elif opcion == "TIPO II":
+    st.write("Has seleccionado Tipo II")
+    st.write("Colágeno estructura tipo II (agregar contenido aquí)")
+elif opcion == "TIPO III":
+    st.write("Has seleccionado TIPO III")
+    st.title("ESTRUCTURAS TIPO III")
+    st.write("bkbalblaba")
+    st.write("Colágeno estructura tipo III")
+    image_comparison(
+        img1="estructuracolag3.jpg",
+        img2="Colag.Tipo3.jpg",
+        label1="Estructura molecular",
+        label2="Estructura",
+    )
 else:
-    print("Opción no válida.")
-
+    st.write("Selecciona una opción del menú.")
 
 #streamlit run proyectofinal.py
